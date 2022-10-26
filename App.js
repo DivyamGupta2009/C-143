6a7086c182b6d9c4bc3eb1b0ccf8d517711f0b58
DivyamGupta2009 <62936715+DivyamGupta2009@users.noreply.github.com>
2022-10-26 22:29:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import HomeScreen from "./screens/Home";
+import { createAppContainer } from "react-navigation";
+
+export default function App() {
+  return <AppContainer />;
+}
+
+
+
+
